439c193bc36193c1bc0048ea1505a87d7a5dc099
Vaka37 <vak-forever@inbox.ru>
2024-05-06 12:34:01 +0300

    app init

---
+++ FurnitureApp/FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Vakil on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
